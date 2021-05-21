c1f81a262a51c87482f6e5e97c0ba6b89c4f10a0
the-golden-girl <dollybhatia777@gmail.com>
2021-05-21 14:05:22 +0530

    4 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,39 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = 'Candidates';
+
+  data = [
+    {
+      id: 1,
+      first_name: 'Jeanettle',
+      last_name: 'Penddreth',
+      email: 'jpenddreth@census.gov',
+      gender: 'Female'
+    },
+    {
+      id: 2,
+      first_name: 'Giavani',
+      last_name: 'Fredlani',
+      email: 'gfredlani@census.gov',
+      gender: 'Male'
+    },
+    {
+      id: 3,
+      first_name: 'Noell',
+      last_name: 'Bea',
+      email: 'nbea@census.gov',
+      gender: 'Female'
+    },
+    {
+      id: 4,
+      first_name: 'Willard',
+      last_name: 'Valek',
+      email: 'wvalek@census.gov',
+      gender: 'Male'
+    }
+  ];
 }
--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,10 +1,89 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1>`,
-  styles: [`h1 { font-family: Lato; }`]
+  templateUrl: './hello.component.html',
+  styles: [
+    `
+      h1 {
+        font-family: Lato;
+      }
+    `
+  ]
 })
-export class HelloComponent  {
-  @Input() name: string;
+export class HelloComponent implements OnInit {
+  @Input() data: any;
+  addForm;
+  userId: number = 4;
+  originalData;
+  isMaleSelected: boolean = false;
+  isFemaleSelected: boolean = false;
+  searchValue: string;
+
+  ngOnInit() {
+    this.originalData = new Object(this.data);
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.addForm = new FormGroup({
+      firstName: new FormControl(''),
+      lastName: new FormControl(''),
+      email: new FormControl(''),
+      gender: new FormControl('')
+    });
+  }
+
+  onAdd() {
+    let userInput = {
+      id: this.userId + 1,
+      first_name: this.addForm.value.firstName,
+      last_name: this.addForm.value.lastName,
+      email: this.addForm.value.email,
+      gender: this.addForm.value.gender
+    };
+    this.data.push(userInput);
+    this.initializeForm();
+  }
+
+  onCancel() {
+    this.initializeForm();
+  }
+
+  onCheckboxSelection(value) {
+    if (value === 'Male') {
+      this.isMaleSelected = this.isMaleSelected === true ? false : true;
+    } else {
+      this.isFemaleSelected = this.isFemaleSelected === true ? false : true;
+    }
+    if (this.isMaleSelected && this.isFemaleSelected) {
+      this.data = this.originalData;
+    } else if (this.isMaleSelected) {
+      this.data = this.data.filter(obj => {
+        return obj.gender === 'Male';
+      });
+    } else if (this.isFemaleSelected) {
+      this.data = this.data.filter(obj => {
+        return obj.gender === 'Female';
+      });
+    } else {
+      this.data = this.originalData;
+    }
+  }
+
+  onSearch() {
+    console.log(this.searchValue);
+    if (this.searchValue && this.searchValue.length > 0) {
+      this.data = this.data.filter(obj => {
+        return (
+          obj.first_name.includes(this.searchValue) ||
+          obj.last_name.includes(this.searchValue) ||
+          obj.email.includes(this.searchValue)
+        );
+      });
+    } else {
+      this.data = this.originalData;
+    }
+  }
 }
